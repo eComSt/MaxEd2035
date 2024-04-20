8ab5f298c876468ae086211927c9912c078fe17b
ecoms <ecoms>
2024-04-20 21:26:17 +0700

    123

--- mod1/lesson4/lists.py
+++ mod1/lesson4/lists.py
@@ -5,6 +5,35 @@ lst3 = list()
 b = 143
 
 lst4 = [1,2,3,4,b, lst2]
-
+words_lst = ['foo','bar']
+# print(words_lst)
 foobar_lst = list('foobar')
-print(foobar_lst[0])
+# print(foobar_lst[-3])
+my_list = [1, 3, 5, 6]
+count = 0
+for item in my_list:
+    count+=1  
+# print(count)
+    
+# my_list = [2,3,4,5,6,7,8,4]
+# my_list[1] = 17
+# print(my_list)
+# del my_list[0]
+# my_list.remove(4)
+# print(my_list)
+# length = len(my_list)
+# # null_count = my_list.count(0)
+# print(length)
+# my_list.append("E52")
+# my_list.insert(1,"HELLO")
+# length = len(my_list)
+# print(my_list)
+# print(length)
+
+
+my_lst = [1,2,3]
+my_lst2 = [4,5,6]
+res_lst = my_lst+my_lst2+[7,8,9]
+
+for i in res_lst:
+    print(i)
