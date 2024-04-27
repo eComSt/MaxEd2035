006a8c904ac30e0567d398fc8661c7ab790bfb8a
ecoms <ecoms>
2024-04-27 20:56:56 +0700

    123

--- mod1/lesson5/hw.py
+++ mod1/lesson5/hw.py
@@ -2,7 +2,7 @@ numbers = input().split()
 sum_numbers = 0
 
 for num in numbers:
-    sum_numbers += int(num)
+    sum_numbers = sum_numbers + int(num)
 
 average = sum_numbers / len(numbers)
 print(average)
---
+++ mod1/lesson5/hw2.py
@@ -0,0 +1,2 @@
+lst = list(map(int, input().split()))
+print(sum(lst)/len(lst))
