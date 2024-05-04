2d1c23921b73a7fb845d56481cca9209786f05b5
ecoms <ecoms>
2024-05-04 21:52:02 +0700

    123

--- mod1/lesson8/hw.py
+++ mod1/lesson8/hw.py
@@ -2,7 +2,7 @@ with open('input.txt') as f:
     data = f.read()
 counter_dict = {}
 for ch in data:
-    if ch not in d:
+    if ch not in counter_dict:
         counter_dict[ch] = 1
     counter_dict[ch] += 1
 with open('answer.txt', 'w') as file:
